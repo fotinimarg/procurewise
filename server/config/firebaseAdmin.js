fb7a02c7d8bebda2ba8bda357aa62638cce3f383
Foteini Margariti Gkini <80010627+fotinimarg@users.noreply.github.com>
2026-01-22 17:59:17 +0200

    Update Firebase Admin SDK configuration

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -1,12 +1,14 @@
 const admin = require("firebase-admin");
 
 // Initialize Firebase Admin SDK
-const serviceAccount = require("./serviceKey.json");
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
 
 if (!admin.apps.length) {
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
-        storageBucket: "thesis-10898.firebasestorage.app",
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     })
 }
 
